a9e6e047dd7a349fe81528910a681d93bef0c0d1
Rachel Dhia <dhiarachella@gmail.com>
2025-11-30 18:54:57 +0700

    color

--- lib/core/config/theme.dart
+++ lib/core/config/theme.dart
@@ -3,12 +3,99 @@ import 'package:flutter/widgets.dart';
 
 class AppColors {
 
-//warna putih untuk ...
-static const Color whiteColor = Color(0xffFFFFFF);
+  // ======= BLUE / COOL TONES =======
+static const Color softBlue = Color(0xFFB3CBFF);
+static const Color pastelBlue = Color(0xFFD2DEEE);
+static const Color icyBlue = Color(0xFFE2FAFF);
+static const Color cloudyBlue = Color(0xFFEAF1FA);
+static const Color lavenderBlue = Color(0xFFAFAFF7);
+static const Color paleSkyBlue = Color(0xFFD6E4F6);
+static const Color mintBlue = Color(0xFFB6F3FF);
 
+// ======= PINK / ROSE TONES =======
+static const Color rosePink = Color(0xFFECCADB);
+static const Color softPink = Color(0xFFFCBEBE);
+static const Color peachPink = Color(0xFFFCC6AF);
+static const Color babyPink = Color(0xFFFFD8F4);
+static const Color blushPink = Color(0xFFFFDBE4);
 
+// ======= ORANGE / WARM TONES =======
+static const Color warmOrange = Color(0xFFF8804B);
+static const Color sunsetOrange = Color(0xFFFE8235);
+static const Color coralOrange = Color(0xFFFE814B);
+static const Color brightOrange = Color(0xFFF77F4A);
 
-}
+// ======= YELLOW / GOLD TONES =======
+static const Color softCream = Color(0xFFFDE99D);
+static const Color paleYellow = Color(0xFFFCFAD9);
+static const Color warmYellow = Color(0xFFFCBC09);
+static const Color lightBanana = Color(0xFFFFF1B7);
+static const Color butterCream = Color(0xFFFFF3C7);
+
+// ======= GREEN TONES =======
+static const Color freshGreen = Color(0xFF8AB239);
+static const Color darkLeafGreen = Color(0xFF53A06E);
+static const Color mintGreen = Color(0xFFB0E9CA);
+
+// ======= GREY / NEUTRAL TONES =======
+static const Color lightGrey = Color(0xFFD9D8D8);
+static const Color lightGrey20 = Color(0x33D9D8D8);
+static const Color lightGrey25 = Color(0x40D9D8D8);
+static const Color smokeGrey = Color(0xFFEBEBEB);
+static const Color frostGrey = Color(0xFFF0F0F0);
+static const Color ghostGrey = Color(0xFFF6F6F6);
+static const Color snowGrey = Color(0xFFFAFAFA);
+static const Color paleGrey = Color(0xFFE2E2E2);
+
+static const Color softGrey = Color(0xFFF4F4F4);
+static const Color boneWhite = Color(0xFFF4F2F1);
+static const Color pearlGrey = Color(0xFFF4F3F1);
+static const Color calmGrey = Color(0xFFF8F6F5);
+static const Color cleanGrey = Color(0xFFFBFBFB);
+
+static const Color stoneGrey = Color(0xFFD6CCC6);
+static const Color mediumGrey = Color(0xFF8A8A8A);
+static const Color graphiteGrey = Color(0xFF707070);
+static const Color charcoalGrey = Color(0xFF292929);
+static const Color darkGrey = Color(0xFF181818);
+static const Color deepGrey = Color(0xFF131313);
+
+// ======= BROWN TONES =======
+static const Color cocoaBrown = Color(0xFF573926);
+static const Color mindfulBrown80 = Color(0xFF433B35);
+static const Color mindfulBrown100Alpha = Color(0xFF363332);
+static const Color mindfulBrown60 = Color(0xFF524036);
+static const Color mindfulBrown10 = Color(0xFF787776);
+static const Color mindfulBrown20 = Color(0xFF726E6C);
+static const Color mindfulBrown100Alpha2 = Color(0xFF373635);
+
+// ======= SPECIAL TONES (from second list) =======
+static const Color optimisticGray80 = Color(0xFF3B3D3F);
+static const Color empathyOrange40 = Color(0xFF7A4B37);
+static const Color empathyOrange30 = Color(0xFF7A5D51);
+static const Color serenityGreen100 = Color(0xFF31332E);
+static const Color serenityGreen50 = Color(0xFF555D42);
+static const Color serenityGreen20 = Color(0xFF71736C);
+static const Color serenityGreen40 = Color(0xFF5F6550);
+static const Color serenityGreen60Alt = Color(0xFF4A5338);
+
+static const Color gentlePurple30 = Color(0xFF57517B);
+static const Color optimisticGray30 = Color(0xFF636568);
+static const Color gray3 = Color(0xFF4C4C4C);
+static const Color zenYellow40 = Color(0xFF7B683E);
+
+// ======= BASIC COLORS =======
+static const Color whiteColor = Color(0xFFFFFFFF);
+static const Color pureBlack = Color(0xFF000000);
+static const Color nightBlack = Color(0xFF070707);
+static const Color inkBlack = Color(0xFF0B0B0B);
+
+ 
+ 
+
+ 
+
+} 
 
 
 
